169346f44706cb435f0a38836dca6f4c5aca17f2
proXDhiya <prox.dhiya@gmail.com>
2020-09-22 14:40:41 +0100

    Solve Home works 10-11

---
+++ 20_07_17/jm_arrint_copy_rev.c
@@ -0,0 +1,25 @@
+// Solve Homework By @proXDhiya
+
+// include libraries
+#include<stdio.h>
+
+// Function prototypes
+int jm_arrint_copy_rev(int *, int *, int);
+
+int main () {
+    int arr1[4] = {26, 4, 489, 7};
+    int arr2[4];
+    arr2[0] = jm_arrint_copy_rev(arr1, arr2, 4);
+    
+    for (int i = 0; i < 4; i++)
+        printf("%d ", arr2[i]);
+    printf("\n");
+    
+    return 0;
+}
+
+int jm_arrint_copy_rev(int arr1[], int arr2[], int size) {
+    for (int i = size - 1; i >= 0; i--)
+        arr2[size - 1 -i] = arr1[i];
+    return *arr2;
+}
---
+++ 20_07_17/jm_arrint_merge.c
@@ -0,0 +1,25 @@
+// Solve Homework By @proXDhiya
+
+// include libraries
+#include<stdio.h>
+
+// Function prototypes
+int jm_arrint_merge(int *, int *, int *, int, int);
+
+int main () {
+    int arr1[3] = {1, 2, 3};
+    int arr2[5] = {4, 5, 6, 7, 8};
+    int arr3[8];
+    arr3[0] = jm_arrint_merge(arr1, arr2, arr3, 3, 5);
+
+    for (int i = 0; i < 5+3; i++)
+        printf("%d ", arr3[i]);
+    printf("\n");
+    return 0;
+}
+
+int jm_arrint_merge(int arr1[], int arr2[], int arr3[], int size1, int size2) {
+    for (int i = 0; i < size1 + size2; i++)
+        arr3[i] = (i < size1) ? arr1[i] : arr2[i - size1];
+    return *arr3;
+}
